fb802af0e6798c29148c6df6930933e5e5b4418f
obvioulescence <clementcolle@free.fr>
2013-10-02 13:42:43 +0200

    Robot project

---
+++ Robot/Robot/Robot.cpp
@@ -0,0 +1,17 @@
+/*
+ * Robot.cpp
+ *
+ * Created: 02/10/2013 13:42:15
+ *  Author: ClementC
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
